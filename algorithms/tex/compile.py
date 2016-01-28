49c69dc0dc3e4dfa1109b3d350f0445a7e378819
Alexey <kha83640@gmail.com>
2016-01-28 22:39:49 +0300

    Добавлен экспериментальный скрипт для автоматической сборки файла со всеми лекциями.

---
+++ algorithms/tex/compile.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# Change coding to UTF-8
+
+# Import section
+import subprocess
+import os
+import re
+
+# Creating temp file with all lectures
+with open('temp.tex', 'w', encoding="utf8") as temp:
+    # Adding header
+    with open('header.tex', 'r', encoding="utf8") as header:
+        temp.write(header.read())
+
+    # Adding necessary lines (beginning of document, title and table of contents)
+    temp.write('\n\\begin{document}\n\n')
+    temp.write('\\maketitle\n\n')
+    temp.write('\\tableofcontents\n\n')
+
+    # Collecting text from separate lecture files
+    lectures = []
+
+    for elem in os.listdir('./'):
+        if os.path.isfile(os.path.join('./', elem)):
+            if elem.startswith('algorithms'):
+                if elem.endswith('.tex'):
+                    lectures.append(elem)
+
+    # Adding it to the temp file
+    for lecture_name in lectures:
+        with open(lecture_name, 'r', encoding='utf8') as lecture:
+            temp_lines = lecture.readlines()
+            for line in temp_lines[3:-1]:
+                temp_line = line.replace('section*', 'section')
+                temp.write(temp_line)
+
+    # Adding the final line
+    temp.write(r'\end{document}')
+
+# In order to create table of contents, I have to compile it twice.
+for _ in range(2):
+    proc = subprocess.Popen(['pdflatex', 'temp.tex'])
+    proc.communicate()
+
+# Renaming the file
+os.rename('temp.pdf', 'algorithms_all_lectures.pdf')
+
+# Removing the litter
+pattern = 'temp*'
+for file in os.listdir('./'):
+    if re.search(pattern, file):
+        os.remove(os.path.join('./', file))
+
+
+
