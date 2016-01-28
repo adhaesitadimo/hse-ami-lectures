b3e58c0a858837d3a4c58f633296709723c502cf
Alexey <kha83640@gmail.com>
2016-01-28 23:10:17 +0300

    Minor script update. Now it saves file to algorithms folder.

--- algorithms/tex/compile.py
+++ algorithms/tex/compile.py
@@ -42,13 +42,14 @@ for _ in range(2):
     proc = subprocess.Popen(['pdflatex', 'temp.tex'])
     proc.communicate()
 
-# If file algorithms_all_lectures.pdf already exists, we will delete it.
+# Saving the file
+os.chdir('..')
 for file in os.listdir('./'):
     if file == 'algorithms_all_lectures.pdf':
         os.remove(os.path.join('./', file))
 
-# Renaming the file
-os.rename('temp.pdf', 'algorithms_all_lectures.pdf')
+os.rename('./tex/temp.pdf', 'algorithms_all_lectures.pdf')
+os.chdir('tex')
 
 # Removing the litter
 pattern = 'temp*'
